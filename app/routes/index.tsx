70ac9bb6d7e4372081681e32efb08986c30b25ef
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2022-10-22 14:33:54 +0000

    add react-query example

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,40 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+type GitHubResponse = {
+  description: string;
+};
+
+// TODO add react suspense
+const Example = () => {
+  const { isLoading, error, data } = useQuery<GitHubResponse>(
+    ["repoData"],
+    () =>
+      fetch("https://api.github.com/repos/tannerlinsley/react-query").then(
+        (res) => res.json()
+      )
+  );
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (error) {
+    return <span>error has occured</span>;
+  }
+
+  return <span>{data?.description}</span>;
+};
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Example />
+    </QueryClientProvider>
   );
 }
